c67d80fdaa6befa024a5a2508d28490c2041bbdb
Andrew Shin <andyrpg@gmail.com>
2023-07-21 13:01:07 -0700

    custom-hookes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { UserInfo } from './UserInfo';
+import { UserInfo } from "./UserInfo";
+import { ProductInfo } from "./ProductInfo";
 
 function App() {
-	return (
-		<UserInfo />
-	);
+  return (
+    <>
+      <UserInfo userId="123" />
+      <UserInfo userId="234" />
+      <UserInfo userId="345" />
+      <ProductInfo productId="1234" />
+    </>
+  );
 }
 
 export default App;
--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,19 +1,42 @@
 import { useCurrentUser } from "./useCurrentUser";
+import { useUser } from "./useUser";
+import { useResource } from "./useResource";
+import { useDataSource } from "./useDataSource";
+import axios from "axios";
 
-export const UserInfo = () => {
-	const user = useCurrentUser();
+const serverResource = (resourceUrl) => {
+  return async () => {
+    const response = await axios.get(resourceUrl);
+    return response.data;
+  };
+};
 
-	const { name, age, hairColor, hobbies } = user || {};
+const localStorageResource = (key) => {
+  return localStorage.getItem(key);
+};
 
-	return user ? (
-		<>
-		<h3>{name}</h3>
-		<p>Age: {age} years</p>
-		<p>Hair Color: {hairColor}</p>
-		<h3>Hobbies:</h3>
-		<ul>
-			{hobbies.map(hobby => <li key={hobby}>{hobby}</li>)}
-		</ul>
-		</>
-	) : <p>Loading...</p>;
-}
+export const UserInfo = ({ userId }) => {
+  // const user = useCurrentUser();
+  //   const user = useUser(userId);
+  // const user = useResource(`/users/${userId}`);
+  const user = useDataSource(serverResource(`/users/${userId}`));
+  // const message = useDataSource(localStorageResource("message"));
+
+  const { name, age, hairColor, hobbies } = user || {};
+
+  return user ? (
+    <>
+      <h3>{name}</h3>
+      <p>Age: {age} years</p>
+      <p>Hair Color: {hairColor}</p>
+      <h3>Hobbies:</h3>
+      <ul>
+        {hobbies.map((hobby) => (
+          <li key={hobby}>{hobby}</li>
+        ))}
+      </ul>
+    </>
+  ) : (
+    <p>Loading...</p>
+  );
+};
